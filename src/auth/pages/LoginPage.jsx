7593248717eddbf311f457befb6b5fc0907e6bdb
itsaalexk <alexkononenko6@protonmail.com>
2023-01-11 23:07:56 +0100

    created RegisterPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom';
 import './LoginPage.css';
 
 export const LoginPage = () => {
@@ -23,57 +24,20 @@ export const LoginPage = () => {
                             />
                         </div>
                         <div className="form-group mb-2">
+                            
                             <input 
                                 type="submit"
                                 className="btnSubmit"
                                 value="Login" 
                             />
+                            
                         </div>
+                        <p className='p-text'>No tienes cuenta? <Link>Registrate</Link></p>
                     </form>
-                </div>
-
-                <div className="col-md-6 login-form-2">
-                    <h3>Registro</h3>
-                    <form>
-                        <div className="form-group mb-2">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Nombre"
-                            />
-                        </div>
-                        <div className="form-group mb-2">
-                            <input
-                                type="email"
-                                className="form-control"
-                                placeholder="Correo"
-                            />
-                        </div>
-                        <div className="form-group mb-2">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Contraseña" 
-                            />
-                        </div>
-
-                        <div className="form-group mb-2">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Repita la contraseña" 
-                            />
-                        </div>
-
-                        <div className="form-group mb-2">
-                            <input 
-                                type="submit" 
-                                className="btnSubmit" 
-                                value="Crear cuenta" />
-                        </div>
-                    </form>
+                   
                 </div>
             </div>
+            
         </div>
     )
 }
